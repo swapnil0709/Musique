9002c9c2751e3942f62d67055cd15d3fb4ed6888
swapnil0709 <swapnilsinha20@gmail.com>
2021-02-24 00:10:55 +0530

    Add Song data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
+import "./styles/app.scss";
+import data from "./data";
 
 function App() {
+  // State
+
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
       <h1>Musicque</h1>
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        currentSong={currentSong}
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+      />
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlay,
@@ -6,7 +6,18 @@ import {
   faAngleRight,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Player = () => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+  const audioRef = useRef(null);
+  const playSongHandler = () => {
+    if (isPlaying) {
+      setIsPlaying(false);
+      audioRef.current.pause();
+    } else {
+      setIsPlaying(true);
+      audioRef.current.play();
+    }
+  };
+
   return (
     <div className="player">
       <div className="time-control">
@@ -16,13 +27,19 @@ const Player = () => {
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
-        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        <FontAwesomeIcon
+          onClick={playSongHandler}
+          className="play"
+          size="2x"
+          icon={faPlay}
+        />
         <FontAwesomeIcon
           className="skip-forward"
           size="2x"
           icon={faAngleRight}
         />
       </div>
+      <audio ref={audioRef} src={currentSong.audio}></audio>
     </div>
   );
 };
