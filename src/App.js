a37617e4cf90573a2446390a3c324093a1010819
swapnil0709 <swapnilsinha20@gmail.com>
2021-02-23 20:32:37 +0530

    Add components Song,Player

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+import Player from "./components/Player";
+import Song from "./components/Song";
+
 function App() {
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <h1>Musicque</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
